85d3c308b14ac1e89e46877ae444e58145fb0356
enogawa <111509163+enogawa@users.noreply.github.com>
2022-12-09 21:58:50 +0900

    readline

--- src/main.c
+++ src/main.c
@@ -3,23 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yshimoda <yshimoda@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: enogawa <enogawa@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 16:52:04 by yshimoda          #+#    #+#             */
-/*   Updated: 2022/12/05 01:32:03 by yshimoda         ###   ########.fr       */
+/*   Updated: 2022/12/09 21:55:44 by enogawa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdlib.h>
 
 int	main(int argc, char const *argv[])
 {
 	(void)argc;
+	char *input;
+
+	while (1)
+	{
+		input = readline(PROMPT);
+		add_history(input);
+		ft_printf("%s\n", input);
+		free(input);
+	}
 	ft_printf("%s\n", argv[0]);
 	return (0);
 }
 
-// __attribute__((destructor)) static void destructor()
-// {
-// 	system("leaks -q minishell");
-// }
+__attribute__((destructor)) static void destructor()
+{
+	system("leaks -q minishell");
+}
