f546fb760b553d8faa98feb49e0cdd1d3ae6d0a3
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2022-12-14 04:03:31 +0900

    fix github action

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: yshimoda <yshimoda@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 16:52:04 by yshimoda          #+#    #+#             */
-/*   Updated: 2022/12/14 03:28:13 by yshimoda         ###   ########.fr       */
+/*   Updated: 2022/12/14 03:59:05 by yshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int	main(int argc, char const *argv[], char **envp)
 
 	(void)argc;
 	(void)envp;
+	printf("%zu\n", ft_strlen(NULL));
 	minishell_signal();
 	while (1)
 	{
--- src/minishell_signal.c
+++ src/minishell_signal.c
@@ -6,7 +6,7 @@
 /*   By: yshimoda <yshimoda@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 15:19:30 by enogawa           #+#    #+#             */
-/*   Updated: 2022/12/14 03:13:59 by yshimoda         ###   ########.fr       */
+/*   Updated: 2022/12/14 03:52:16 by yshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 void	minishell_signal(void)
 {
-	return (0);
+	return ;
 }
