d14ca92c22a5391e2486c5724c25892556c921b0
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2023-02-09 19:26:20 +0900

    Step_2 Build and Test

---
+++ src/test.sh
@@ -0,0 +1,44 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#                                                       :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: yshimoda <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/02/08 21:44:51 by yshimoda          #+#    #+#              #
+#    Updated: 2023/02/09 18:21:03 by yshimoda         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+assert()
+{
+	# show test case
+	printf '%-30s:' "\"$1\""
+
+	# save bash output to cmp
+	echo -n -e "$1" | bash >cmp 2>&-
+	#save bash exit status to expected
+	expected=$?
+
+	# save minishell output to out
+	echo -n -e "$1" | ./minishell >out 2>&-
+	#save minishell exit status to actual
+	actual=$?
+
+	# compare bash, minishell output
+	diff cmp out >/dev/null && echo -n '  diff OK' || echo -n '  diff NG'
+
+	# compare bash, minishell exit status
+	if [ "$actual" = "$expected" ]; then
+		echo -n '  status OK'
+	else
+		echo -n "  status NG, expected $expected but got $actual"
+	fi
+	echo
+}
+
+assert ''
+assert ""
+assert "/bin/pwd"
+assert "/bin/echo"
+
