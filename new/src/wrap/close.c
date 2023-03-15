90bd4dc5c4fe04d97908dd139ffe1374ddf568e8
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2023-03-15 21:44:37 +0900

    tmp pipe

--- new/src/wrap/close.c
+++ new/src/wrap/close.c
@@ -6,7 +6,7 @@
 /*   By: yshimoda <yshimoda@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 19:38:57 by yshimoda          #+#    #+#             */
-/*   Updated: 2023/03/12 20:28:56 by yshimoda         ###   ########.fr       */
+/*   Updated: 2023/03/15 21:06:47 by yshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,28 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int	wrap_close(int fd)
-{
-	int	result;
+// int	wrap_close(int fd)
+// {
+// 	int	result;
+// 
+// 	result = close(fd);
+// 	if (result == -1)
+// 	{
+// 		perror(NULL);
+// 		assert_error("close\n");
+// 	}
+// 	return (result);
+// }
 
-	result = close(fd);
-	if (result == -1)
-	{
-		perror(NULL);
-		assert_error("close\n");
-	}
-	return (result);
+int wrap_close(int fd)
+{
+    int ret;
+
+    ret = close(fd);
+    if (ret == -1)
+    {
+        perror("close");
+        g_return_error.exec_error = true;
+    }
+    return (ret);
 }
