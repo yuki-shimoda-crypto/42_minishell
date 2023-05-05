87e2b766fb36f0b38b581fc6f05ee01d76c00e31
yuki-shimoda-crypto <77478109+yuki-shimoda-crypto@users.noreply.github.com>
2023-05-05 17:50:47 +0900

    fix: export unset fix export, unset return value and errorhandling when something other than under score, alphabet, number , make an error fixes #127 fixes #120

--- src/builtin/unset.c
+++ src/builtin/unset.c
@@ -12,22 +12,45 @@
 
 #include "minishell.h"
 
+static bool is_str_alpha_num_under_unset(const char *str)
+{
+	size_t	i;
+	
+	i = 1;
+	while (str[0] && str[i])
+	{
+		if (!is_alpha_num_under(str[i]))
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
 int	builtin_unset(char **argv, t_env **env_list)
 {
 	size_t	i;
+	int		status;
 
 	if (!argv[1])
 		return (0);
 	i = 1;
+	status = 0;
 	while (argv[i])
 	{
-		if (strchr(argv[i], '='))
+		if (!is_alpha_under(argv[i][0]))
 		{
-			unset_error(argv[i]);
-			return (1);
+			status = unset_error(argv[i]);
+			i++;
+			continue ;
+		}
+		if (!is_str_alpha_num_under_unset(argv[i]))
+		{
+			status = unset_error(argv[i]);
+			i++;
+			continue ;
 		}
 		del_env(argv[i], env_list);
 		i++;
 	}
-	return (0);
+	return (status);
 }
