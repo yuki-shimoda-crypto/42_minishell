27e53d21afece808580c74f1b9227d79ccfdd8fc
enogawa <111509163+enogawa@users.noreply.github.com>
2023-05-05 12:40:31 +0900

    fix: signal_handle_\n Issue : #118

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -335,7 +335,8 @@ void	wait_child_process(void)
 			g_return_error.return_value = WEXITSTATUS(status);
 		else if (WIFSIGNALED(status))
 		{
-			wrap_write(STDOUT_FILENO, "\n", 1);
+			if (isatty(STDIN_FILENO))
+				wrap_write(STDOUT_FILENO, "\n", 1);
 			g_return_error.g_sig = 0;
 			if (WTERMSIG(status) == SIGINT)
 				g_return_error.ctrl_c = true;//
